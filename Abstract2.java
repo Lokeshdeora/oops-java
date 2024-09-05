b1db0f8bbbeeb3a3b679675d327461fffb778122
Lokesh Deora <lokeshdeorajodhpur@gmail.com>
2024-09-05 17:17:37 +0530

    Add files via upload

--- Abstract2.java
+++ Abstract2.java
@@ -1,64 +1,32 @@
-//abstract class 
-//	no return and no argument :abstract 
+//abstract class :
+//	constructor :default 
+//	 return and no argument : abstract method
+//	 return and  argument : abstract method
 
-//abstract class 
-//	no return and  argument :abstract 
+//class B extends abstract class 
 
-//abstract class 
-//	 return and no argument :abstract 
 
-//abstract class 
-//	 return and  argument :abstract 
-
-//abstract class 
-//	 covariant return type :abstract 
-
-//class extends abstract class 
-
-//Main classs 
-//access
-//multi level inheritance
-abstract class A {
-    abstract void show();
-}
-abstract class B extends A{
-    abstract void show(int a);
-}
-abstract class C extends B{
-    abstract int display();
-}
-abstract class D extends C{
+//class Main
+abstract class A{
+    A(){
+       System.out.println("Abstract Class Default Constructor");
+    }   
+    abstract int show();
     abstract int display(int a);
-}
-abstract class E extends D{
-    abstract E disp();
-}
-class Test extends E{
-    void show(){
-        System.out.println("Abstract class A method");
-    }
-    void show(int a){
-        System.out.println("Abstract class B method"+a);
-    }
-    int display(){
-       System.out.println("Abstract class C method");
-       return 0;
-    }
-    int display(int a){
-        return a;
-    }
-    E disp(){
-        System.out.println("Covariant return type");
-        return this;
-    }
-}
-public class Abstract2 {
+   }
+   class B extends A{
+       int show(){
+           System.out.println("abstract method with return & No Argument");
+           return 0;
+       }
+       int display(int a){
+           return a;
+       }
+   }
+   public class Abstract2 {
     public static void main(String[] args) {
-        Test t = new Test();
-        t.show();
-        t.show(3);
-        t.display();
-        System.out.println("Abstract class D method"+t.display(8));
-        t.disp();
+        B b = new B();
+        b.show();
+        System.out.println("abstract method with Return & With Argument "+b.display(10));
     }
 }
