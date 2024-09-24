630b5bc4b5c00b24666b74352050eea26e165d1b
Lokesh Deora <lokeshdeorajodhpur@gmail.com>
2024-09-24 18:50:49 +0530

    Add files via upload

---
+++ Static2.java
@@ -0,0 +1,14 @@
+//outer class  non static and inner class static and method  non static : 
+class A{
+    static class B{
+        void show(){
+            System.out.println("non-static method inside static inner class");
+        }
+    }
+}
+public class Static2 {
+    public static void main(String[] args) {
+        A.B a = new A.B();
+        a.show();
+    }
+}
