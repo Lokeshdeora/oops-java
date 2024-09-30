a13f62ff14573326198854b0dc9793a0aa868065
Lokesh Deora <lokeshdeorajodhpur@gmail.com>
2024-09-30 23:52:04 +0530

    Add files via upload

---
+++ TypeCasting5.java
@@ -0,0 +1,11 @@
+//long -> float
+//long -> double 
+
+public class TypeCasting5 {
+    public static void main(String[] args) {
+        long l = 1234656;
+        double d = l;
+        System.out.println("long: "+l);
+        System.out.println("double: "+d);
+    }
+}
